7d7661aa15854c78ac0e2c37b2ee8e80ed9d5df0
Yahir-Juarez <idv22c.yjuarez@uartesdigitales.edu.mx>
2024-08-15 20:35:04 -0600

    Structs

--- D3DWnd/Skeleton.h
+++ D3DWnd/Skeleton.h
@@ -4,10 +4,29 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-class Skeleton
+class Bone
 {
 public:
+	int id = 0; // position of the bone in final upload array
+	std::string name = "";
+	//glm::mat4 offset = glm::mat4(1.0f);
+	std::vector<Bone> children = {};
+};
 
+struct BoneTransformTrack {
+	std::vector<float> positionTimestamps = {};
+	std::vector<float> rotationTimestamps = {};
+	std::vector<float> scaleTimestamps = {};
+
+	//std::vector<glm::vec3> positions = {};
+	//std::vector<glm::quat> rotations = {};
+	//std::vector<glm::vec3> scales = {};
+};
+
+struct Animation {
+	float duration = 0.0f;
+	float ticksPerSecond = 1.0f;
+	std::unordered_map<std::string, BoneTransformTrack> boneTransforms = {};
 };
 
 //bool nodeIsBone(const aiNode* node, const aiScene* scene) {
